1b3959248a8c2f1ee553e73830a47568d1b8a8c4
abhishek-pstu <115950126+abhishek-pstu@users.noreply.github.com>
2024-11-22 02:49:15 +0600

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<!--Footer section starts -->
+        <div class="footer">
+           <div class="wrapper">
+               <p class="text-center">2020 All rights reserved,Food House.Developed by <a href="#">Abhishek Das</a></p>
+           </div>
+        </div>
+        <!--Footer section ends -->
+    </body>
+</html>
---
+++ admin/partials/login-check.php
@@ -0,0 +1,16 @@
+<?php
+
+//chck the user logged in or not
+
+
+if(!isset($_SESSION['user'])){
+
+    $_SESSION['no-login-message']="<div class='error text-center'>Please login to access Admin Panel.</div>";
+
+    header('location:'.SITEURL.'admin/login.php');
+
+}
+ 
+
+
+?>
